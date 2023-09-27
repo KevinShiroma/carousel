1645e462dc861c5ff7c216ad50b56d21535025df
Kevin Shiroma <makoto.shiroma@hotmail.com>
2023-09-27 11:28:06 -0300

    eventos de touch adicionados

--- js/carousel.js
+++ js/carousel.js
@@ -20,24 +20,38 @@ export default class Carousel {
   }
 
   aoComecar(event) {
-    event.preventDefault();
-    this.dist.startX = event.clientX;
-    this.wrapper.addEventListener("mousemove", this.aoMover);
+    let movetype;
+    if (event.type === "mousedown") {
+      event.preventDefault();
+      this.dist.startX = event.clientX;
+      movetype = "mousemove";
+    } else {
+      this.dist.startX = event.changedTouches[0].clientX;
+      movetype = "touchmove";
+    }
+    this.wrapper.addEventListener(movetype, this.aoMover);
   }
 
   aoMover(event) {
-    const posicaoFinal = this.posicaoAtualizada(event.clientX);
+    const pointerPosition =
+      event.type === "mousemove"
+        ? event.clientX
+        : event.changedTouches[0].clientX;
+    const posicaoFinal = this.posicaoAtualizada(pointerPosition);
     this.moveImg(posicaoFinal);
   }
 
   aoTerminar(event) {
-    this.wrapper.removeEventListener("mousemove", this.aoMover);
+    const movetype = event.type === "mouseup" ? "mousemove" : "touchmove";
+    this.wrapper.removeEventListener(movetype, this.aoMover);
     this.dist.posicaoFinal = this.dist.movePosition;
   }
 
   addCarouselEvents() {
     this.wrapper.addEventListener("mousedown", this.aoComecar);
+    this.wrapper.addEventListener("touchstart", this.aoComecar);
     this.wrapper.addEventListener("mouseup", this.aoTerminar);
+    this.wrapper.addEventListener("touchend", this.aoTerminar);
   }
 
   bindEvents() {
